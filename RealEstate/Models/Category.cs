eb419bd9df8f5866c21b42dca9607569e21cff74
SteAlb89 <98278002+SteAlb89@users.noreply.github.com>
2023-09-07 17:38:11 +0200

    Create Models Folder and add Category Class Model and add propetery to the model

---
+++ RealEstate/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace RealEstate.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
