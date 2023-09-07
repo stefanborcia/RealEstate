89afe430ae8d87f63f7bb9e8645f32f78cac69cd
SteAlb89 <98278002+SteAlb89@users.noreply.github.com>
2023-09-07 17:44:21 +0200

    add a list of categories and initialized them

--- RealEstate/Controllers/CategoriesController.cs
+++ RealEstate/Controllers/CategoriesController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using RealEstate.Models;
 
 namespace RealEstate.Controllers
 {
@@ -7,6 +8,11 @@ namespace RealEstate.Controllers
     [ApiController]
     public class CategoriesController : ControllerBase
     {
-
+        private List<Category> categories = new List<Category>()
+        {
+            new Category { Id = 1,Name="Apartment", ImageUrl = "apartment.png"},
+            new Category { Id = 2,Name="Commercial", ImageUrl = "commercial.png"},
+            new Category { Id = 3,Name="House", ImageUrl = "house.png"}
+        };
     }
 }
