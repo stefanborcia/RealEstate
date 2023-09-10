b05d0cebfaf13b69acd68a0e3a2de55810d0860d
SteAlb89 <98278002+SteAlb89@users.noreply.github.com>
2023-09-10 21:09:33 +0200

    DbContext and DbSet Properties

---
+++ RealEstate/Data/ApiDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RealEstate.Models;
+
+namespace RealEstate.Data
+{
+    public class ApiDbContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(local);Database=RealEstateDb;");
+        }
+    }
+}
