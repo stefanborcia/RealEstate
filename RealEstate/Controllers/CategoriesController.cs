3bc48fe3271cc8c73cbd02c4066244c684dcecbb
SteAlb89 <98278002+SteAlb89@users.noreply.github.com>
2023-09-17 18:34:08 +0200

    Create CategoriesController and add Get method

---
+++ RealEstate/Controllers/CategoriesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RealEstate.Data;
+
+namespace RealEstate.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private ApiDbContext _apiDbContext = new ApiDbContext();
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_apiDbContext.Categories);
+        }
+    }
+}
