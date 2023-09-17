c031bda827e2325ba3006611399f0bd6895d8640
SteAlb89 <98278002+SteAlb89@users.noreply.github.com>
2023-09-17 17:31:16 +0200

    refact dbContext

--- RealEstate/Models/Category.cs
+++ RealEstate/Models/Category.cs
@@ -10,7 +10,6 @@ namespace RealEstate.Models
         public string Name { get; set; }
         [Required(ErrorMessage = "Image url can't be null or empty")]
         public string ImageUrl { get; set; }
-        public string Description { get; set; }
         public ICollection<Property>Properties { get; set; } // 1-Many relationship between Category and Property
     }
 }
