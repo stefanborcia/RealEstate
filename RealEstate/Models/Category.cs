bc24ecdd722d9b89dbde8adc68d7d585dc5ad270
SteAlb89 <98278002+SteAlb89@users.noreply.github.com>
2023-09-17 17:03:34 +0200

    Create property model class

--- RealEstate/Models/Category.cs
+++ RealEstate/Models/Category.cs
@@ -11,5 +11,6 @@ namespace RealEstate.Models
         [Required(ErrorMessage = "Image url can't be null or empty")]
         public string ImageUrl { get; set; }
         public string Description { get; set; }
+        public ICollection<Property>Properties { get; set; } // 1-Many relationship between Category and Property
     }
 }
