b48ab13308fb6aab475f0021d0900ef59c9861d9
SteAlb89 <98278002+SteAlb89@users.noreply.github.com>
2023-09-17 17:42:24 +0200

    Create UserController and implement Register method as HttpPost

---
+++ RealEstate/Controllers/UsersController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RealEstate.Data;
+using RealEstate.Models;
+
+namespace RealEstate.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    { 
+        ApiDbContext _apiDbContext = new ApiDbContext();
+
+        [HttpPost]
+        public IActionResult Register([FromBody] User user)
+        {
+           var userExists = _apiDbContext.Users.FirstOrDefault(u => u.Email == user.Email);
+           if (userExists == null)
+           {
+               return BadRequest("User with same email already exists");
+           }
+           _apiDbContext.Users.Add(user);
+           _apiDbContext.SaveChanges();
+           return StatusCode(StatusCodes.Status201Created);
+        }
+    }
+}
