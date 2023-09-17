3c2d0d94a184dfe436c1f323094987337411906f
SteAlb89 <98278002+SteAlb89@users.noreply.github.com>
2023-09-17 16:55:53 +0200

    Create a user model class

--- RealEstate/Models/Category.cs
+++ RealEstate/Models/Category.cs
@@ -1,9 +1,14 @@
-﻿namespace RealEstate.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RealEstate.Models
 {
     public class Category
     {
+        [Required]  
         public int Id { get; set; }
+        [Required(ErrorMessage = "Category name can't be null or empty")]  // validation
         public string Name { get; set; }
+        [Required(ErrorMessage = "Image url can't be null or empty")]
         public string ImageUrl { get; set; }
         public string Description { get; set; }
     }
