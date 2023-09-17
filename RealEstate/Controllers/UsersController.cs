1a698b046e61a2523de34a62e95908e75ef60f1b
SteAlb89 <98278002+SteAlb89@users.noreply.github.com>
2023-09-17 18:15:06 +0200

    Implement Login method and generate JWT

--- RealEstate/Controllers/UsersController.cs
+++ RealEstate/Controllers/UsersController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using RealEstate.Data;
 using RealEstate.Models;
 
@@ -9,7 +13,13 @@ namespace RealEstate.Controllers
     [ApiController]
     public class UsersController : ControllerBase
     { 
-        ApiDbContext _apiDbContext = new ApiDbContext();
+        readonly ApiDbContext _apiDbContext = new ApiDbContext();
+        private readonly IConfiguration _configuration;
+
+        public UsersController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
 
         [HttpPost]
         public IActionResult Register([FromBody] User user)
@@ -22,6 +32,32 @@ namespace RealEstate.Controllers
            _apiDbContext.Users.Add(user);
            _apiDbContext.SaveChanges();
            return StatusCode(StatusCodes.Status201Created);
+        }
+
+        [HttpPost("[action]")]
+        public IActionResult Login([FromBody] User user)
+        {
+            var currentUser =
+                _apiDbContext.Users.FirstOrDefault(u => u.Email == user.Email && u.Password == user.Password);
+            if (currentUser == null)
+            {
+                return NotFound("User is not found");
+            }
+
+           var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Key"]));
+           var credentials =  new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+           var claims = new[]
+           {
+               new Claim(ClaimTypes.Email, user.Email)
+           };
+           var token = new JwtSecurityToken(
+               issuer: _configuration["JWT:Issuer"],
+               audience: _configuration["JWT: Audience"],
+               claims: claims,
+               expires:DateTime.Now.AddMinutes(60),
+               signingCredentials:credentials);
+           var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+           return Ok(jwt);
         }
     }
 }
