783dfa53454f3c9403ed57971c9134ad59dc47a3
SteAlb89 <98278002+SteAlb89@users.noreply.github.com>
2023-09-18 19:10:47 +0200

    Create PropertiesController and add a post method

---
+++ RealEstate/Controllers/PropertiesController.cs
@@ -0,0 +1,40 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RealEstate.Data;
+using RealEstate.Models;
+
+namespace RealEstate.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PropertiesController : ControllerBase
+    {
+        private ApiDbContext _dbContext = new ApiDbContext();
+
+        [HttpPost]
+        [Authorize]
+        public IActionResult Post([FromBody] Property property)
+        {
+            if (property == null)
+            {
+                return NoContent();
+            }
+            else
+            {
+              var userEmail = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Email)?.Value;
+              var user = _dbContext.Users.FirstOrDefault(u => u.Email == userEmail);
+              if (user == null)
+              {
+                  return NotFound();
+                  property.IsTrending = false;
+                  property.UserId=user.Id;
+                  _dbContext.Properties.Add(property);
+                  _dbContext.SaveChanges();
+              }
+              return StatusCode(StatusCodes.Status201Created);
+            }
+        }
+    }
+}
